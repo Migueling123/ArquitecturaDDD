6a23700c6939560b9b97ec615370e99e9acf45e9
Migueling123 <miguelabr.ing@gmail.com>
2020-04-03 15:28:45 -0500

    Agregar archivos de proyecto.

---
+++ WebApiTestDDD/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTestDDD
+{
+    public class Class1
+    {
+    }
+}
